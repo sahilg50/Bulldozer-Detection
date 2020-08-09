2b02153b5e7e46ff0c7bb03a6426d5ee47fb288d
sahilg50 <56653026+sahilg50@users.noreply.github.com>
2020-08-09 12:09:58 +0530

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+void sample_function()
+{
+    cout << "This is a sample function to check the github version control";
+}
+
+int main()
+{
+    cout << "This code was written to check the git functionality";
+
+    return 0;
+}
